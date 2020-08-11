bdcf001cbb76baa8283c0b108bf4ea3fd9db0f98
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-11 16:36:55 -0500

    create a now image found image

--- src/App/RecipeListItem.js
+++ src/App/RecipeListItem.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {ListGroupItem} from 'reactstrap';
 
 const RecipeListItem = props => {
+    let preview = (<img src={`./images/0.png`} />);
+    if(props.recipe.images.length > 0)
+        preview = (<img src={`./images/${props.recipe.id}/${props.recipe.images[0]}`} />);
+        
     return (
         <ListGroupItem key={props.recipe.id} className=" recipeListItem row">
             <figure className="col-1">
-                <img src={`./images/${props.recipe.id}/${props.recipe.images[0]}`} />
+                {preview}
             </figure>
             <section className="col-11">
                 <h3>{props.recipe.title}</h3>
