368988919f8da25b4b1cc4a15610a641714508bd
Malotkya <ajmalotky@madisoncollege.edu>
2020-08-11 14:07:08 -0500

    view recipe page

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,56 +1,68 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
+import {Alert} from 'reactstrap';
 
-class Recipe extends Component {
-    state = { recipe: {
+const Recipe = props => {
+    const [recipe, setRecipe] = useState({
             items: [],
             ingredients: [],
-            directions: []
-        }
-    };
+            directions: [],
+            images: []
+    });
 
-    componentDidMount() {
-        fetch('/Recipe')
-            .then(res => res.json())
-            .then(recipe => {
-                recipe.images = [
-                    "1.jpeg",
-                    "2.jpeg",
-                    "3.jpeg"
-                ]
-                this.setState({ recipe });
-            })
+    const [visible, setVisible] = useState(false);
+    const [alert, setAlert] = useState("");
+    const onDismiss = () => setVisible(false);
+    const displayAlert = message => {
+        setAlert(message);
+        setVisible(true);
     }
 
-    render() {
-        return (
-            <main className="recipe" id={this.state.recipe.id}>
-                <div className="title">
-                    <h2>{this.state.recipe.title}</h2>
-                    <span>
-                        <img src="./images/1/1.jpeg" />
-                    </span>
-                </div>
-                <h3>Items:</h3>
-                <h3>Ingredients:</h3>
-                <h3>Directions:</h3>
-                    <ul>
-                        {this.state.recipe.items.map(item =>
-                            <li>{item}</li>
-                        )}
-                    </ul>
-                    <ul>
-                        {this.state.recipe.ingredients.map(ingredient =>
-                            <li>{ingredient}</li>
-                        )}
-                    </ul>
-                    <ol>
-                        {this.state.recipe.directions.map(direction =>
-                            <li>{direction}</li>
-                        )}
-                    </ol>
-            </main>
-        )
-    };
+    useEffect(() => {
+        fetch(`/Recipe/${props.id}`)
+            .then(async(res) => {
+                if(!res.ok)
+                    throw await res.json();
+
+                setRecipe(await res.json());
+            }).catch(e =>{
+                console.error(e);
+                displayAlert(e.message);
+            })
+    }, []);
+
+    return (
+        <main className="recipe" id={recipe.id}>
+            <Alert color="danger" isOpen={visible} toggle={onDismiss}>
+                {alert}
+            </Alert>
+            <div className="title">
+                <h2>{recipe.title}</h2>
+                <figure>
+                    {recipe.images.map(img =>
+                        <img src={`./images/${recipe.id}/${img}`} />
+                    )}
+                </figure>
+            </div>
+            <h3>Items:</h3>
+            <h3>Ingredients:</h3>
+            <h3>Directions:</h3>
+            <ul>
+                {recipe.items.map(item =>
+                    <li>{item}</li>
+                )}
+            </ul>
+            <ul>
+                {recipe.ingredients.map(ingredient =>
+                    <li>{ingredient}</li>
+                )}
+            </ul>
+            <ol>
+                {recipe.directions.map(direction =>
+                    <li>{direction}</li>
+                )}
+            </ol>
+        </main>
+    )
 }
 
 export default Recipe;
